31aa93a35b58f4d0814184a9be95de1f7a753df3
vamsi-web <102210130+vamsi-web@users.noreply.github.com>
2024-12-02 15:01:46 +0530

    Update image_train.py

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -3,7 +3,8 @@ Train a diffusion model on images.
 """
 import sys
 import os
-sys.path.append(os.path.abspath('./'))
+#sys.path.append(os.path.abspath('/content/guided-diffusion'))
+sys.path.append('/kaggle/working/guided-diffusion')
 import argparse
 
 from guided_diffusion import dist_util, logger
@@ -65,7 +66,7 @@ def create_argparser():
         schedule_sampler="uniform",
         lr=1e-4,
         weight_decay=0.0,
-        lr_anneal_steps=0,
+        lr_anneal_steps=10000,
         batch_size=1,
         microbatch=-1,  # -1 disables microbatches
         ema_rate="0.9999",  # comma-separated list of EMA values
